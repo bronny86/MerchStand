971ce3c5a96b4ec6be14cfdde1581786cdd1eee6
Bronny <bronnyhamilton@gmail.com>
2025-03-12 15:31:56 +1100

    feat: added seeds.js

--- src/server.js
+++ src/server.js
@@ -1,15 +1,11 @@
-// Make the .env data ready for use.
 const dotenv = require('dotenv');
 dotenv.config();
 
-// Import the Express package and configure some needed data.
 const express = require('express');
 const app = express();
-// If no process.env.X is found, assign a default value instead.
 const HOST = process.env.HOST || 'localhost';
 const PORT = process.env.PORT || 3000;
 
-// Configure some basic Helmet settings on the server instance.
 const helmet = require('helmet');
 app.use(helmet());
 app.use(helmet.permittedCrossDomainPolicies());
@@ -20,11 +16,6 @@ app.use(helmet.contentSecurityPolicy({
     }
 }));
 
-// Configure some basic CORS settings on the server instance.
-// These origin values don't actually have to be anything - 
-// this project exists without a front-end, but any front-end
-// that should interact with this API should be listed in the 
-// array of origins for CORS configuration.
 const cors = require('cors');
 var corsOptions = {
     origin: ["http://localhost:5000", "https://deployedApp.com"],
@@ -32,7 +23,6 @@ var corsOptions = {
 }
 app.use(cors(corsOptions));
 
-// Configure some API-friendly request data formatting.
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
@@ -40,10 +30,10 @@ const mongoose = require('mongoose');
 var databaseURL = "";
 switch (process.env.NODE_ENV?.toLowerCase()) {
     case "test":
-        databaseURL = "mongodb://localhost";
+        databaseURL = "mongodb://localhost:27017/test-database";
         break;
     case "development":
-        databaseURL = "mongodb://localhost";
+        databaseURL = "mongodb://localhost:27017/development-database";
         break;
     case "production":
         databaseURL = process.env.DATABASE_URL;
@@ -52,6 +42,9 @@ switch (process.env.NODE_ENV?.toLowerCase()) {
         console.error("Incorrect JS environment specified, database will not be connected.");
         break;
 }
+
+console.log(`Connecting to database at: ${databaseURL}`);
+
 const { databaseConnector } = require('./database.js');
 
 databaseConnector(databaseURL).then(() => {
@@ -63,10 +56,6 @@ databaseConnector(databaseURL).then(() => {
     `);
 });
 
-
-// Return a bunch of useful details from the database connection
-// Dig into each property here:
-// https://mongoosejs.com/docs/api/connection.html
 app.get("/databaseHealth", (request, response) => {
     let databaseState = mongoose.connection.readyState;
     let databaseName = mongoose.connection.name;
@@ -81,22 +70,30 @@ app.get("/databaseHealth", (request, response) => {
     })
 });
 
+app.get("/databaseDump", async (request, response) => {
+    const dumpContainer = {};
 
+    var collections = await mongoose.connection.db.listCollections().toArray();
+    collections = collections.map((collection) => collection.name);
 
-// Add a route just to make sure things work.
-// This path is the server API's "homepage".
+    for (const collectionName of collections) {
+        let collectionData = await mongoose.connection.db.collection(collectionName).find({}).toArray();
+        dumpContainer[collectionName] = collectionData;
+    }
+
+    console.log("Dumping all of this data to the client: \n" + JSON.stringify(dumpContainer, null, 4));
+
+    response.json({
+        data: dumpContainer
+    });
+});
+
 app.get('/', (request, response) => {
     response.json({
         message:"Hello world!"
     });
 });
 
-
-
-
-// Keep this route at the end of this file, only before the module.exports!
-// A 404 route should only trigger if no preceding routes or middleware was run. 
-// So, put this below where any other routes are placed within this file.
 app.get('*', (request, response) => {
     response.status(404).json({
         message: "No route with that path found!",
@@ -104,7 +101,6 @@ app.get('*', (request, response) => {
     });
 });
 
-// Export everything needed to run the server.
 module.exports = {
     HOST,
     PORT,
