cf3f8628723f36a35e76a37f7b9943545e4ab2d1
root <root@DESKTOP-L1CLIL1>
2025-03-16 18:02:56 +1100

    auth hotfix

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
 
-// Define the login route
+// POST /auth/login
 router.post('/login', authController.login);
 
 module.exports = router;
