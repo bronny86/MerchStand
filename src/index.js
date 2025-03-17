8e6413fb12187de145e3847f98e72f2df9bc19ae
root <root@DESKTOP-L1CLIL1>
2025-03-17 20:14:40 +1100

    removed data dump added to index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
+const mongoose = require('mongoose');
 
 // Importing routes
 const { databaseConnector } = require('./database');  // Import the database connection
@@ -12,7 +13,6 @@ const paymentRoutes = require('./routes/paymentRoutes.js');  // Import payment r
 const clipartRoutes = require('./routes/clipartRoutes.js');  // Import clipart routes
 const designRoutes = require('./routes/designRoutes.js');  // Import design routes
 const stockRoutes = require('./routes/stockRoutes.js');  // Import stock routes
-const datadumpRoutes = require('./routes/datadumpRoutes.js');
 
 const app = express();
 const HOST = process.env.HOST || 'localhost';
@@ -48,11 +48,30 @@ app.use('/payments', paymentRoutes);  // Use the payment routes
 app.use('/cliparts', clipartRoutes);  // Use the clipart routes
 app.use('/designs', designRoutes);  // Use the design routes
 app.use('/stocks', stockRoutes);  // Use the stock routes
-app.use('/datadump', datadumpRoutes);  // Use the data dump route
 
 // Default route for the root path returning JSON
 app.get('/', (req, res) => {
     res.json({ message: 'Welcome to MerchStand!' });
+});
+
+// /databaseDump route to return data from actual database
+app.get('/databaseDump', async (req, res) => {
+    try {
+        const dumpContainer = {};
+        let collections = await mongoose.connection.db.listCollections().toArray();
+        collections = collections.map((collection) => collection.name);
+
+        for (const collectionName of collections) {
+            let collectionData = await mongoose.connection.db.collection(collectionName).find({}).toArray();
+            dumpContainer[collectionName] = collectionData;
+        }
+
+        console.log("Dumping database data: \n" + JSON.stringify(dumpContainer, null, 4));
+        res.json({ data: dumpContainer });
+    } catch (error) {
+        console.error("❌ Error fetching database dump:", error);
+        res.status(500).json({ message: "Error fetching database dump", error: error.message });
+    }
 });
 
 // Export app for testing
--- src/routes/dataDumpRoutes.js
+++
@@ -1,9 +0,0 @@
-// routes/ datadumpRoutes.js
-const express = require('express');
-const router = express.Router();
-const dataDumpController = require('../controllers/datadumpController'); // Import controller
-
-// Define the route for dumping the database data
-router.get('/databaseDump', dataDumpController.getDatabaseDump);
-
-module.exports = router;
